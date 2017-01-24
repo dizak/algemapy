dea45d50d29c1c86bb7e0850a38d9f0ebcfb2351
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2017-01-24 13:21:04 +0100

    ENH@argparse:headnode args

--- algemapy.py
+++ algemapy.py
@@ -36,6 +36,65 @@ def main():
                                                   Facilitates non-16S markers\
                                                   analysis.",
                                      version="testing")
+    headnode.add_argument("--partition",
+                          action="store",
+                          dest="partition",
+                          metavar="",
+                          default="long",
+                          help="headnode's partition. Values: test, short, big,\
+                                  long, accel. Accel necessary for phi/gpu nodes\
+                                  Default <long>.")
+    headnode.add_argument("--nodes",
+                          action="store",
+                          dest="nodes",
+                          metavar="",
+                          default=1,
+                          help="number of nodes. Default: <1>.")
+    headnode.add_argument("--ntasks-per-node",
+                          action="store",
+                          dest="ntasks_per_node",
+                          metavar="",
+                          default=6,
+                          help="number of tasks to invoke on each node.\
+                                  Default <6>")
+    headnode.add_argument("--mem-per-cpu",
+                          action="store",
+                          dest="mem_per_cpu",
+                          metavar="",
+                          default=24,
+                          help="maximum amount of real memory per node in\
+                                  gigabytes. Default <24>.")
+    headnode.add_argument("--node-list",
+                          action="store",
+                          dest="node_list",
+                          metavar="",
+                          default=None,
+                          help="request a specific list of nodes")
+    headnode.add_argument("--processors",
+                          action="store",
+                          dest="processors",
+                          metavar="",
+                          default=24,
+                          help="number of logical processors. Default: <24>")
+    headnode.add_argument("--resources",
+                          action="store",
+                          dest="resources",
+                          metavar="",
+                          default=None,
+                          help="shortcut for headnode's resources\
+                                  reservation. Accepted values are: <S>mall,\
+                                  <M>edium, <L>arge, <XL>arge for regular nodes\
+                                  with mpi. <PHI> for single phi node, <JUMBO>\
+                                  for two phi nodes. Overrides all the other\
+                                  headnode arguments. Use if you are lazy.")
+    headnode.add_argument("--notify-email",
+                          action="store",
+                          dest="notify_email",
+                          metavar="",
+                          default=None,
+                          help="email address you want to notify when job is\
+                                  done.")
+
     args = parser.parse_args()
 
 
