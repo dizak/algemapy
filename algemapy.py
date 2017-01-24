c1dde955c883a4970265464a48f90e9a7ed81f3e
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2017-01-24 13:03:58 +0100

    ENH:Defs for jj2

--- algemapy.py
+++ algemapy.py
@@ -6,6 +6,25 @@ import argparse
 import os
 
 
+def load_template_file(template_file):
+    template_Loader = jj2.FileSystemLoader(searchpath="/")
+    template_Env = jj2.Environment(loader=template_Loader)
+    template = template_Env.get_template(template_file)
+    return template
+
+
+def render_template(template_loaded):
+    template_vars = {}
+    template_rendered = template_loaded.render(template_vars)
+    return template_rendered
+
+
+def save_template(out_file_name,
+                  template_rendered):
+    with open(out_file_name, "w") as fout:
+        fout.write(template_rendered)
+
+
 __author__ = "Dariusz Izak IBB PAS"
 __version = "testing"
 
