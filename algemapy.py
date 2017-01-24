25c24feb6035e38a90f2b6aa70419966d0c2b179
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2017-01-24 13:00:40 +0100

    Initial commit

---
+++ algemapy.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+
+import jinja2 as jj2
+import argparse
+import os
+
+
+__author__ = "Dariusz Izak IBB PAS"
+__version = "testing"
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
