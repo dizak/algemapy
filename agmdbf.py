42ccb6da49fc151eef4fe9ca5b890be643a622b4
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2017-02-17 11:03:03 +0100

    ENH:basic argparse

--- agmdbf.py
+++ agmdbf.py
@@ -9,5 +9,28 @@ __author__ = "Dariusz Izak IBB PAS"
 __version = "testing"
 
 
+def main():
+    parser = argparse.ArgumentParser(prog="agmdbf",
+                                     usage="agmdbf.py [FILE] [OPTION]",
+                                     description="Part of \
+                                     ALternativeGEnomicMAppingPYpeline.\
+                                     Holds algemapy built-in database\
+                                     formatting.",
+                                     version="testing")
+    parser.add_argument(action="store",
+                        dest="files_directory",
+                        metavar="",
+                        help="Input file path.")
+    parser.add_argument("-o",
+                        "--output",
+                        action="store",
+                        dest="output_path",
+                        metavar="",
+                        default=".",
+                        help="Output file path. Default: working\
+                        directory")
+    args = parser.parse_args()
+
+
 if __name__ == '__main__':
     main()
