b8a8a20188cdc6f2f0c842873e2681b0fb6097cf
dizak <dariusz.izak@ibb.waw.pl>
2017-04-06 14:54:05 +0200

    ENH@Func for sanitizing refseqs names from gene db

--- agmdbf.py
+++ agmdbf.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 
+import re
 import argparse
 from Bio import Entrez, SeqIO, SeqRecord, Phylo
 import pandas as pd
@@ -11,9 +12,9 @@ __author__ = "Dariusz Izak IBB PAS"
 __version = "testing"
 
 
-def db_dwn(db,
+def db_dwn(output_file_name,
+           db,
            term,
-           output_file_name,
            email="dariusz.izak@ibb.waw.pl"):
     Entrez.email = email
     db_handle = Entrez.esearch(db=db, term=term)
@@ -26,24 +27,6 @@ def db_dwn(db,
             hand = Entrez.efetch(db=db, id=i, rettype="fasta", retmode="text")
             rec = SeqIO.read(hand, "fasta")
             SeqIO.write(rec, fout, "fasta")
-
-
-def gene_seq_dwn(output_file_name,
-                 sum_df,
-                 rettype="fasta"):
-    with open(output_file_name, "w") as fout:
-        for i in sum_df.itertuples():
-            id = getattr(i, "ACC_NO")
-            start = getattr(i, "START")
-            end = getattr(i, "END")
-            hand = Entrez.efetch(db="nucleotide",
-                                 id=id,
-                                 seq_start=start,
-                                 seq_stop=end,
-                                 rettype=rettype,
-                                 retmode="text")
-            rec = SeqIO.read(hand, rettype)
-            SeqIO.write(rec, fout, rettype)
 
 
 def id_reform(input_file_name,
@@ -59,25 +42,61 @@ def id_reform(input_file_name,
             SeqIO.write(record, fout, "fasta")
 
 
-def tab_sum_reform(input_file_name,
-                   sep="\t",
-                   cols2rename={"genomic_nucleotide_accession.version":
-                                "ACC_NO",
-                                "start_position_on_the_genomic_accession":
-                                "START",
-                                "end_position_on_the_genomic_accession":
-                                "END"},
-                   essenatial_cols=["ACC_NO", "START", "END"]):
+def gene_tab_sum_reform(input_file_name,
+                        sep="\t",
+                        cols2rename={"genomic_nucleotide_accession.version":
+                                     "ACC_NO",
+                                     "start_position_on_the_genomic_accession":
+                                     "START",
+                                     "end_position_on_the_genomic_accession":
+                                     "END"},
+                        essenatial_cols=["ACC_NO", "START", "END"]):
     tab_sum_df = pd.read_csv(input_file_name, sep=sep)
-    tab_sum_ren.rename(columns=columns2rename)
+    tab_sum_ren = tab_sum_df.rename(columns=cols2rename)
     tab_sum_nona = tab_sum_ren[essenatial_cols].dropna()
-    tab_sum_nona.ACC_NO = tab_sum_nona.ACC_NO.map(lambda x: x.replace(".1",
-                                                                      ""))
-    tab_sum_nona.START = tab_sum_nona.START.map(lambda x: str(x).replace(".0",
-                                                                         ""))
-    tab_sum_nona.END = tab_sum_nona.END.map(lambda x: str(x).replace(".0",
-                                                                     ""))
+    tab_sum_nona.ACC_NO = tab_sum_nona.ACC_NO.map(lambda x: re.sub("\.\d+",
+                                                                   "",
+                                                                   x))
+    tab_sum_nona.START = tab_sum_nona.START.map(lambda x: re.sub("\.\d+",
+                                                                 "",
+                                                                 str(x)))
+    tab_sum_nona.END = tab_sum_nona.END.map(lambda x: re.sub("\.\d+",
+                                                             "",
+                                                             str(x)))
     return tab_sum_nona
+
+
+def gene_seq_dwn(output_file_name,
+                 sum_df,
+                 rettype="fasta"):
+    with open(output_file_name, "w") as fout:
+        for i in tqdm(sum_df.itertuples()):
+            id = getattr(i, "ACC_NO")
+            start = getattr(i, "START")
+            end = getattr(i, "END")
+            hand = Entrez.efetch(db="nucleotide",
+                                 id=id,
+                                 seq_start=start,
+                                 seq_stop=end,
+                                 rettype=rettype,
+                                 retmode="text")
+            rec = SeqIO.read(hand, rettype)
+            SeqIO.write(rec, fout, rettype)
+
+
+def gene_sanit_desc(input_file_name,
+                    output_file_name,
+                    file_format):
+    def f(x):
+        x_split = x.split(" ")[1:3]
+        x_split_str = " ".join(x_split)
+        return x_split_str
+    with open(input_file_name) as fin:
+        with open(output_file_name, "w") as fout:
+            for i in tqdm(SeqIO.parse(fin, file_format)):
+                i.id = ""
+                i.description = f(i.description)
+                SeqIO.write(i, fout, file_format)
 
 
 def main():
